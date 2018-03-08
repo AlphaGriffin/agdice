38d6d433e3115a527980c2012e586b3cf64668b5
Shawn Wilson <shawn@lannocc.com>
2018-03-07 18:58:01 -0700

    testing basic injection

---
+++ js/inject.js
@@ -0,0 +1,13 @@
+
+
+//alert('AlphaGriffin Dice mod loaded');
+
+
+$('h1').text('Alpha Griffin Dice ready');
+
+$('h1').append('<button id="ag_start">Start</button>');
+
+$('#ag_start').click(function() {
+    $('.bet').val('you clicked it');
+});
+
---
+++ js/popup_ag.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+
+    $('#start').click(function() {
+        $('#start').text('yo');
+    });
+});
+
+
