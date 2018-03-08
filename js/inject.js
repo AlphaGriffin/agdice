e11e348ac9d191adf81ec70681932147078252e7
Shawn Wilson <shawn@lannocc.com>
2018-03-07 19:21:27 -0700

    now randomly clicks a roll

--- js/inject.js
+++ js/inject.js
@@ -8,6 +8,23 @@ $('h1').text('Alpha Griffin Dice ready');
 $('h1').append('<button id="ag_start">Start</button>');
 
 $('#ag_start').click(function() {
-    $('.bet').val('you clicked it');
+    //$('.bet').val('you clicked it');
+    ag_bet();
 });
+
+
+function ag_bet() {
+    bet = $('.bet').val();
+
+    // randomly choose one of the bet buttons
+    btn = Math.floor(Math.random() * 2);
+    if (btn < 1) {
+        btn = $('input[value="Roll < 48"]');
+    }
+    else {
+        btn = $('input[value="Roll > 52"]');
+    }
+
+    btn.click();
+}
 
